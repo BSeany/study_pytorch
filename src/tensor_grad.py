60e5a7de023904b531c5a669d08be4db2ceab3fb
Sean Yu <yu_chenglong@hotmail.com>
2026-02-21 12:31:46 +0800

    chapter 1: what is tensor

---
+++ src/tensor_grad.py
@@ -0,0 +1,12 @@
+import torch
+
+# 创建一个需要梯度的张量
+tensor_requires_grad = torch.tensor([1.0], requires_grad=True)
+
+# 进行一些操作
+tensor_result = tensor_requires_grad * 2
+print(tensor_result)
+
+# 计算梯度
+tensor_result.backward()
+print(tensor_requires_grad.grad)  # 输出梯度
